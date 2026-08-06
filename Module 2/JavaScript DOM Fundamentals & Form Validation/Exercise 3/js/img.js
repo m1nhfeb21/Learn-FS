20f6c194ee7be16d189c0474eb9bc05c81f111ce
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-06 10:23:24 +0700

    Create img.js

---
+++ Module 2/JavaScript DOM Fundamentals & Form Validation/Exercise 3/js/img.js
@@ -0,0 +1,9 @@
+let changeimg = document.getElementById("ChangeSource");
+let ChangeBtn = document.getElementById("changeimageBtn");
+
+ChangeBtn.addEventListener("click", function () {
+  changeimg.setAttribute(
+    "src",
+    "https://kenh14cdn.com/2020/7/17/brvn-15950048783381206275371.jpg",
+  );
+});
