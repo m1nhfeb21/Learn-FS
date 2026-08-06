0c10807a24c9e430e432defe0cc157cebef6c69b
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-06 08:14:41 +0700

    Create content.js

---
+++ Module 2/JavaScript DOM Fundamentals & Form Validation/Exercise 1/js/content.js
@@ -0,0 +1,4 @@
+let getMainTitle = document.getElementById("main-title");
+
+getMainTitle.textContent = "Welcome to DOM Session";
+getMainTitle.innerHTML += '<span style="color:red;"> Hello</span>';
