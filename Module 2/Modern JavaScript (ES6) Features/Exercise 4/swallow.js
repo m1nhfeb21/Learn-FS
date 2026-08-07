f6c8587748ab0869a705c6285c4b4f3504f6427b
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-07 11:24:59 +0700

    Create swallow.js

---
+++ Module 2/Modern JavaScript (ES6) Features/Exercise 4/swallow.js
@@ -0,0 +1,8 @@
+const oldHardware = ["CPU", "RAM"];
+
+const newHardware = ["SSD", "GPU"];
+
+const allHardware = [...oldHardware, ...newHardware, "Monitor"];
+// const allHardware = [[oldHardware] + [newHardware] + "Monitor"];
+
+// console.log(allHardware);
