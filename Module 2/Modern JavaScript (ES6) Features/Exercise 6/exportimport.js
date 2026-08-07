85b436ac4ffe18143a6fbca8cf5b9cb2b4780079
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-07 15:07:09 +0700

    Create exportimport.js

---
+++ Module 2/Modern JavaScript (ES6) Features/Exercise 6/exportimport.js
@@ -0,0 +1,8 @@
+// import * as math from "./mathUtils.js";
+
+import { add, substract } from "./mathUtils.js";
+let a = Number(prompt("Nhập a:"));
+let b = Number(prompt("Nhập b:"));
+
+console.log(add(a, b));
+console.log(substract(a, b));
