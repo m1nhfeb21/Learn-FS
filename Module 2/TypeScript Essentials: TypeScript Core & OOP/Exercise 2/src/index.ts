245d37519a873e37da17adfc3001190bd3e19e34
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-09 13:55:47 +0700

    Create index.ts

---
+++ Module 2/TypeScript Essentials: TypeScript Core & OOP/Exercise 2/src/index.ts
@@ -0,0 +1,12 @@
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+let newUser: User = {
+  id: 1,
+  name: "Minh",
+  //   email: "minh@gmail.com",
+  minhpro: "minh@gmail.com"
+};
