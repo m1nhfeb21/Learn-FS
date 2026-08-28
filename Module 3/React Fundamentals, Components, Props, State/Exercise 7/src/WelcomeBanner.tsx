929eed8b216160676de9d4bb218a9cac017372d0
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-08-28 21:08:48 +0700

    Create WelcomeBanner.tsx

---
+++ Module 3/React Fundamentals, Components, Props, State/Exercise 7/src/WelcomeBanner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState } from "react";
+import Loggin from "./Loggin";
+
+export default function WelcomeBanner() {
+  return (
+    <div>
+      <h2>Chào mừng trở lại!</h2>
+    </div>
+  );
+}
