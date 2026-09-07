ae8c802b0333470fbbab71049bbbd29147ec4d3c
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-09-07 21:36:07 +0700

    Create App.tsx

---
+++ Module 3/React Form Handling & Validation/Exercise 2/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Update from "./Update";
+import Touched from "./Touched";
+
+function App() {
+  return (
+    <div>
+      <Touched />
+    </div>
+  );
+}
+
+export default App;
