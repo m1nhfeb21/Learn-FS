2013535ad8acebddc3f10e585316227d14625659
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-09-08 23:01:42 +0700

    Create App.tsx

---
+++ Module 3/React Form Handling & Validation/Exercise 3/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordForm from "./PasswordForm";
+
+function App() {
+  return (
+    <div>
+      <PasswordForm></PasswordForm>
+    </div>
+  );
+}
+
+export default App;
