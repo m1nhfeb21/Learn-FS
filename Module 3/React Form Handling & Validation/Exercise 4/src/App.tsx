7d0528b8303ec3536663c5121aa47e373dbd6801
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-09-09 22:11:53 +0700

    Create App.tsx

---
+++ Module 3/React Form Handling & Validation/Exercise 4/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DoubleSubmit from "./Double-Submit";
+
+function App() {
+  return (
+    <div>
+      <DoubleSubmit />
+    </div>
+  );
+}
+
+export default App;
