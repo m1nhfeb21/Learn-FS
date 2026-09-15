e8538f4276a7a26f146fe176edf7d85a45aa0849
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-09-15 20:18:22 +0700

    Create App.tsx

---
+++ Module 3/React Hooks, Custom Hooks & Routing/Exercise 6/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import CourseSearch from "./components/modules/CourseSearch";
+
+function App() {
+  return (
+    <div>
+      <CourseSearch></CourseSearch>
+    </div>
+  );
+}
+
+export default App;
