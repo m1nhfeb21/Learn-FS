ed145c6ea6135f946126727439861c31229d4de4
Minh Nguyen Ngoc <24521068@gm.uit.edu.vn>
2026-09-20 10:29:17 +0700

    Add NewsletterButton component for subscription

---
+++ Module 3/Testing & Deployment in ReactJS/Exercise 4/src/NewsletterButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewsletterButton() {
+  return (
+    <div>
+      <button type="submit">Đăng ký nhận tin</button>
+    </div>
+  );
+}
+
+export default NewsletterButton;
